ee6be798db18cfd45239b49b65e12f9ef08d9d22
wojtekw92 <wwozniak92@gmail.com>
2014-11-14 15:20:01 +0100

    menu refactor and reaction to window resize added

--- scripts/main.js
+++ scripts/main.js
@@ -1,56 +1,6 @@
-/*var dimension = [document.documentElement.clientWidth, document.documentElement.clientHeight];
-var c = document.getElementById("mainCanvas");
-c.width = dimension[0];
-c.height = dimension[1];
-var ctx = c.getContext("2d");
-ctx.font = "60px Orbitron";
-ctx.fillStyle = "#FFFFFF";
-textWidth = ctx.measureText("pureJSpong").width;
-ctx.fillText("pureJSpong",(c.width/2) - (textWidth / 2),100);
-ctx.font = "40px Orbitron";
-textWidth = ctx.measureText(">1 Player<").width;
-ctx.fillText(">1 Player<",(c.width/2) - (textWidth / 2),200);
-ctx.fillStyle = "#999999";
-textWidth = ctx.measureText("2 Players").width;
-ctx.fillText("2 Players",(c.width/2) - (textWidth / 2),240);
-textWidth = ctx.measureText("Options").width;
-ctx.fillText("Options",(c.width/2) - (textWidth / 2),280);
-var writeText=function(text,size,top,color){
-    ctx.fillStyle = color;
-    ctx.font = size+"px Orbitron";
-    textWidth = ctx.measureText(text).width;
-    ctx.fillText(text,(c.width/2) - (textWidth / 2),top);
-}*/
-var menu = [{font:60, text:"pureJSpong",color:"#FFFFFF", top:100},{font:40, text:"1 Player",color:"#999999", top:200},{font:40, text:"2 Players",color:"#999999", top:240},{font:40, text:"Options",color:"#999999", top:280}];
-//document.onkeydown = checkKey;
-var pom=1;
-/*function checkKey(e) {
-
-    e = e || window.event;
-
-    if (e.keyCode == '38') {
-        ctx.clearRect(0,0,c.width,c.height);
-        pom--;
-        if(pom<1)pom=3;
-        for(var i=0;i<4;i++) {
-            if(i!=pom)writeText(menu[i].text,menu[i].font,menu[i].top,menu[i].color);
-            else writeText(">"+menu[i].text+"<",menu[i].font,menu[i].top,"#FFFFFF");
-        }
-    }
-    else if (e.keyCode == '40') {
-        ctx.clearRect(0,0,c.width,c.height);
-            pom++;
-            if(pom>3)pom=1;
-        for(var i=0;i<4;i++) {
-            if(i!=pom)writeText(menu[i].text,menu[i].font,menu[i].top,menu[i].color);
-            else writeText(">"+menu[i].text+"<",menu[i].font,menu[i].top,"#FFFFFF");
-        }
-        // down arrow
-    }
-}*/
-//====Screen class basic methods and hooks
 var canvasScreen = (function() {
   var keyHooks = [];
+  var resizeHooks = [];
   var canvasElement;
   var canvasContex;
 
@@ -60,8 +10,24 @@ var canvasScreen = (function() {
     canvasElement.height= document.documentElement.clientHeight;
     canvasContex= canvasElement.getContext("2d");
     document.onkeydown = checkHook;
+    window.onresize = resizeScreen;
   }
-
+  var reInit = function() {
+    keyHooks = [];
+    resizeHooks = [];
+    canvasElement.width = document.documentElement.clientWidth;
+    canvasElement.height= document.documentElement.clientHeight;
+  }
+  var resizeScreen = function() {
+    canvasElement.width = document.documentElement.clientWidth;
+    canvasElement.height= document.documentElement.clientHeight;
+    for (i = 0; i < resizeHooks.length; i++) {
+      resizeHooks[i]();
+    }
+  }
+  var addResizeHook = function(functionName) {
+    resizeHooks.push(functionName);
+  }
 
   var addKeyHook=function(code,functionName) {
     keyHooks.push({key:code, func:functionName});
@@ -91,33 +57,109 @@ var canvasScreen = (function() {
 
   return {
     init: init,
+    reInit: reInit,
     cleanScreen: cleanScreen,
     putText: putText,
-    addKeyHook: addKeyHook
+    addKeyHook: addKeyHook,
+    addResizeHook: addResizeHook
   }
+})();
+var onePlayer=null;
+var options=null
+var twoPlayers=function(){
+  canvasScreen.reInit();
+  canvasScreen.addKeyHook(27,function(){
+    canvasScreen.reInit();
+    canvasScreen.addKeyHook(40,keyDown);
+    canvasScreen.addKeyHook(38,keyUp);
+    canvasScreen.addKeyHook(13,menuWrapper.execute);
+    canvasScreen.addResizeHook(menuWrapper.printMenu);
+    menuWrapper.printMenu();
+  });
+}
+var menuWrapper =(function(){
+  var menu = [
+    {
+        font:60,
+        text:"pureJSpong",
+        color:"#FFFFFF",
+        hoverText: "",
+        hoverColor: "",
+        top:100,
+        hover: 0,
+        click: null
+    },
+    {
+        font:40,
+        text:"1 Player",
+        color:"#999999",
+        hoverText: ">1 Player<",
+        hoverColor: "#FFFFFF",
+        top:200,
+        hover: 1,
+        click: onePlayer
+    },
+    {
+        font:40,
+        text:"2 Players",
+        color:"#999999",
+        hoverText: ">2 Players<",
+        hoverColor: "#FFFFFF",
+        top:240,
+        hover: 1,
+        click: twoPlayers
+    },
+    {
+        font:40,
+        text:"Options",
+        color:"#999999",
+        hoverText: ">Options<",
+        hoverColor: "#FFFFFF",
+        top:280,
+        hover: 1,
+        click: options
+    }
+  ];
+  var pom=1;
+  var printMenu=function(val)//0-refresh 1-down -1 up
+  {
+    val = typeof val !== 'undefined' ? val : 0;
+    canvasScreen.cleanScreen();
+    if(val==1) {
+      pom++;
+      if(pom==menu.length) pom=0;
+    }
+    else if(val==-1){
+      pom--;
+      if(pom==-1) pom=menu.length-1;
+    }
+    for(var i=0;i<menu.length;i++) {
+      if(i==pom && menu[i].hover==0) pom++;
+      if(i!=pom)canvasScreen.putText(menu[i].font,menu[i].color,menu[i].text,menu[i].top,"center");
+      else canvasScreen.putText(menu[i].font,menu[i].hoverColor,menu[i].hoverText,menu[i].top,"center");
+    }
+
+  }
+  var execute=function() {
+    menu[pom].click();
+  }
+  return {
+    execute: execute,
+    printMenu: printMenu
+  }
+
+
 })();
 
 var keyUp = function() {
-    canvasScreen.cleanScreen();
-    pom--;
-        if(pom<1)pom=3;
-        for(var i=0;i<4;i++) {
-            if(i!=pom)canvasScreen.putText(menu[i].font,menu[i].color,menu[i].text,menu[i].top,"center");
-            else canvasScreen.putText(menu[i].font,"#FFFFFF",">"+menu[i].text+"<",menu[i].top,"center");
-        }
+      menuWrapper.printMenu(-1);
 }
 var keyDown = function() {
-    canvasScreen.cleanScreen();
-    pom++;
-            if(pom>3)pom=1;
-        for(var i=0;i<4;i++) {
-            if(i!=pom)canvasScreen.putText(menu[i].font,menu[i].color,menu[i].text,menu[i].top,"center");
-            else canvasScreen.putText(menu[i].font,"#FFFFFF",">"+menu[i].text+"<",menu[i].top,"center");
-        }
+    menuWrapper.printMenu(1);
 }
-
 canvasScreen.init("canvas");
 canvasScreen.addKeyHook(40,keyDown);
 canvasScreen.addKeyHook(38,keyUp);
-keyUp();
-keyDown();
+canvasScreen.addKeyHook(13,menuWrapper.execute);
+canvasScreen.addResizeHook(menuWrapper.printMenu);
+menuWrapper.printMenu();
